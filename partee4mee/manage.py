07b259c6616f3cb54f7fa2c9b2c3faf77f10bf91
SmiejaGrzegorz <g.smieja@op.pl>
2020-11-21 17:22:55 +0100

    start project

--- partee4mee/manage.py
+++ partee4mee/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'partee4mee.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sda.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
