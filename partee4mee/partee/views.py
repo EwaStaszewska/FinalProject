02bcd8bb0a59a8b9100ed52b60b348bcb892f7f2
bvbkuba <sikora.maciej1992@gmail.com>
2020-11-23 01:31:48 +0100

    changing main in views.py. I added a form to search by city in main.html using views.py

--- partee4mee/partee/views.py
+++ partee4mee/partee/views.py
@@ -4,7 +4,29 @@ from .forms import PartyForm
 
 def main(request):
     all_paty = Party.objects.all()
-    return render(request, "main.html", {'all':all_paty})
+
+
+    # searching by form
+    city_contains_query = request.GET.get('filter_by_city')
+    # city_contains_query is a var in form in main.html.
+    all_city_query = Party.objects.all() 
+
+    if city_contains_query != '' and city_contains_query is not None:
+        all_city_query = all_city_query.filter(adress__icontains= city_contains_query)
+    
+    #end searching
+
+
+    # for our knowledge: 
+    # party_filter = Party.objects.filter(date__startswith ='2020-11-22') 
+    # startswith, because we've in db datetimefield. I suppose it'll be useful in our next_steps
+    context = {
+        'all':all_paty,
+        'city_filter': all_city_query
+        # 'filter_party': party_filter,
+    }
+
+    return render(request, "main.html",context)
 
 
 def add(requset):
