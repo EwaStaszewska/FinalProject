8a808a866534ad0857c1d577c0ecddcbfe176bc5
bvbkuba <sikora.maciej1992@gmail.com>
2020-11-26 22:18:41 +0100

    ms

--- partee4mee/partee/forms.py
+++ partee4mee/partee/forms.py
@@ -1,8 +1,9 @@
 from django.forms import ModelForm
 from .models import Party
+from django import forms
 
-class PartyForm(ModelForm):
-    
+
+class PartyForm(forms.ModelForm): 
     class Meta:
         model = Party  
         fields = ["date", "city", "party_type", "free_space",
