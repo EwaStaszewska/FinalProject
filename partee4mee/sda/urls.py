464b57d0cc0ac8630ed68273ddf1556db90617f4
Ewa <ewa.n.staszewska@gmail.com>
2020-11-28 11:04:41 +0100

    new apps accounts for authentication

--- partee4mee/sda/urls.py
+++ partee4mee/sda/urls.py
@@ -20,6 +20,7 @@ from partee.views import main, add
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', main, name="main"),
-    path('add_party/', add, name="add_event")
+    path('add_party/', add, name="add_event"),
+    path ('accounts/', include('accounts.urls'))
 ]
 
