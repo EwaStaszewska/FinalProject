b351dc7d761f49207b50b4103ac2107d4a31becd
bvbkuba <sikora.maciej1992@gmail.com>
2020-12-13 08:33:14 +0100

    final version

--- partee4mee/sda/urls.py
+++ partee4mee/sda/urls.py
@@ -16,19 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from partee.views import main, add, your_account, party_signed_up_by_user, user_signed_up_events, party_signed_off_by_user,\
-    error_site_signed, edit_event, edit_event_2, delete_event_by_user
+    error_site_signed, edit_event, edit_event_2, delete_event_by_user,access_dismiss
 
 urlpatterns = [
-    path ('', main),
     path('admin/', admin.site.urls),
     path('main/', main, name="main"),
     path('add_party/', add, name="add_event"),
-    path('delete_party/<int:pk>', delete_event_by_user, name=""),
+    path('delete_party/<int:pk>', delete_event_by_user, name="delete_event"),
     path('your_account/',your_account , name="your_account"),
     path('signed_up_events/<int:pk>', party_signed_up_by_user, name="party_signed_up_by_user"),
     path('user_signed_up_events/', user_signed_up_events, name="user_signed_up_events"),
     path('user_signed_off_events/<int:pk>', party_signed_off_by_user, name="party_signed_off_by_user"),
     path('error/', error_site_signed, name="error_site_signed"),
+    path('access_dismiss/', access_dismiss, name="access_dismiss"),
     path('edit_event/<int:id>/', edit_event, name="edit"),
     path('edit_event_2/<int:id>/', edit_event_2, name="edit_2"),
     path ('accounts/', include('accounts.urls')),
